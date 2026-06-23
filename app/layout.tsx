d35e2d308f871472923be3515a9f311f73671a82
maneldourado <jorjaoemanuel71985@gmail.com>
2026-06-23 15:50:57 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+// app/layout.tsx
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'CHMS - Sistema de Gestão de Saúde Ocupacional',
+  description: 'Sistema de Gestão de Saúde Ocupacional',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
